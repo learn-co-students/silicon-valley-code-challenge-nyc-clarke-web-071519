49540b721e255c5e8ce8874ef1b0730e8587c833
Valentino Wong <valentino.wong@gmail.com>
2019-07-23 00:44:49 -0400

    Done.

--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,52 @@
 class VentureCapitalist
 
+    #   VentureCapitalist#name returns a **string** that is the venture capitalist's name
+    #   VentureCapitalist#total_worth returns a **number** that is the total worth of this investor (e.g., think of it as how much money they have)
+    attr_reader :name, :total_worth
+
+    @@all = []
+
+    def initialize(name, total_worth)
+        @name = name
+        @total_worth = total_worth
+
+        @@all << self
+    end
+
+    #   VentureCapitalist.all returns an array of all venture capitalists
+    def self.all
+        @@all
+    end
+
+    #   VentureCapitalist.tres_commas_club returns an array of all venture capitalists in the Trés Commas club (they have more then 1,000,000,000 `total_worth`)
+    def self.tres_commas_club
+        self.all.select {|venture_capitalist| venture_capitalist.total_worth > 1000000000}
+    end
+
+    #   VentureCapitalist#offer_contract given a **startup object**, type of investment (as a string), and the amount invested (as a float), creates a new funding round and associates it with that startup and venture capitalist.
+    def offer_contract(startup, investment_type, amount_invested)
+        FundingRound.new(investment_type, amount_invested, startup, self)
+    end
+    
+    #   VentureCapitalist#funding_rounds returns an array of all funding rounds for that venture capitalist
+    def funding_rounds
+        FundingRound.all.select {|funding_round| funding_round.venture_capitalist == self}
+    end
+    
+    #   VentureCapitalist#portfolio Returns a **unique** list of all startups this venture capitalist has funded
+    def portfolio
+        funding_rounds.map {|funding_round| funding_round.startup}.uniq
+    end
+    
+    #   VentureCapitalist#biggest_investment returns the largest funding round given by this venture capitalist
+    def biggest_investment
+        funding_rounds.max_by {|funding_round| funding_round.investment}
+    end
+    
+    #   VentureCapitalist#invested given a **domain string**, returns the total amount invested in that domain
+    def invested(domain)
+        invested_funding_rounds_for_this_domain = funding_rounds.select {|funding_round| funding_round.startup.domain == domain}
+        invested_funding_rounds_for_this_domain.reduce(0) {|total, funding_round| total + funding_round.investment}
+    end
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,33 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+# Test Data
+# Startups
+airbnb = Startup.new("Airbnb", "Brian Chesky", "Home Rentals")
+uber = Startup.new("Uber", "Travis Kalanick", "Ride Sharing")
+spotify = Startup.new("Spotify", "Daniel Ek", "Music Streaming")
+# Venture Capitalists
+y_combinator = VentureCapitalist.new("Y Combinator", 80000000000)
+sequoia_capital = VentureCapitalist.new("Sequoia Capital", 1000000000000)
+greylock_partners = VentureCapitalist.new("Greylock Partners", 3500000000)
+crunch_fund = VentureCapitalist.new("Crunch Fund", 940000000)
+founders_fund = VentureCapitalist.new("Founders Fund", 3000000000)
+first_round_capital = VentureCapitalist.new("First Round Capital", 70000000000)
+northzone = VentureCapitalist.new("Northzone", 900000000)
+wellington_partners = VentureCapitalist.new("Wellington Partners", 800000000)
+# Funding Rounds
+airbnb_pre_seed = FundingRound.new("Pre-Seed", 20000, airbnb, y_combinator)
+airbnb_seed = FundingRound.new("Seed", 600000, airbnb, sequoia_capital)
+airbnb_series_a = FundingRound.new("Series A", 7200000, airbnb, greylock_partners)
+airbnb_series_b = FundingRound.new("Series B", 112000000, airbnb, crunch_fund)
+airbnb_series_c = FundingRound.new("Series C", 200000000, airbnb, founders_fund)
+uber_seed = FundingRound.new("Seed", 1250000, uber, first_round_capital)
+uber_series_a = FundingRound.new("Series A", 11000000, uber, first_round_capital)
+uber_series_b = FundingRound.new("Series B", 32000000, uber, crunch_fund)
+spotify_series_a = FundingRound.new("Series A", 21640000, spotify, northzone)
+spotify_series_b = FundingRound.new("Series B", 50000000, spotify, wellington_partners)
+spotify_series_c = FundingRound.new("Series C", 75000000, spotify, founders_fund)
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
